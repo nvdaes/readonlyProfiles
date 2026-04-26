dba15531f27e506ced5ab61e2edd81df70b6017c
Noelia Ruiz Martínez <nrm1977@gmail.com>
2026-04-26 07:54:27 +0200

    Update script

--- .github/scripts/crowdinSync.ps1
+++ .github/scripts/crowdinSync.ps1
@@ -1,6 +1,54 @@
 #!/usr/bin/env pwsh
 $ErrorActionPreference = 'Stop'
+# Config git
+git config user.name "github-actions[bot]"
+git config user.email "github-actions[bot]@users.noreply.github.com"
 
+# Update xliff file
+$xlifFile = "$addonId.xliff"
+$mdFile = "./readme.md"
+if (Test-Path $mdFile) {
+    if (Test-Path $xliffFile) {
+        $tempXliff = [System.IO.Path]::GetTempFileName()
+        Copy-Item "$addonId.xliff" $tempXliff -Force
+        Write-Host "Copied $addonId.xliff to temporary file: $tempXliff"
+        uv run .github/scripts/markdownTranslate.py updateXliff -m $mdFile -x $tempXliff -o $xliffFile  
+        Write-Host "Updated $xlifFile based on $mdFile"
+    } else {
+        Write-Host "XLIFF file not found, but readme.md exists. Creating an XLIFF template for translations."
+        uv run .github/scripts/markdownTranslate.py generateXliff -m $mdFile -o $xliffFile
+    }
+} else {
+    Write-Host "readme.md not found. Skipping XLIFF generation."
+}
+
+# Update pot file in Crowdin
+scons pot
+$potFile = "$addonId.pot"
+if (Test-Path $potFile) {
+    Write-Host "Uploading updated POT to Crowdin..."
+    ./l10nUtil.exe uploadSourceFile "$potFile" -c addon
+} else {
+    Write-Host "POT file not found, skipping POT update."
+}
+
+# Update xliff file in Crowdin
+if (Test-Path $xliffFile) {
+    Write-Host "Uploading XLIFF to Crowdin..."
+    ./l10nUtil.exe uploadSourceFile "$xliffFile" -c addon
+    git add "$xliffFile"
+    git diff --staged --quiet
+    if ($LASTEXITCODE -ne 0) {
+        git commit -m "Update $xliffFile for $addonId"
+        git push
+      } else {
+        Write-Host "No changes to $xliffFile, skipping commit."
+    }
+} else {
+    Write-Host "XLIFF file not found, skipping XLIFF upload."
+}
+
+# Export translations
 write-host "Exporting translations from Crowdin..."
 ./l10nUtil.exe exportTranslations -o _addonL10n -c addon
 
@@ -79,11 +127,7 @@ foreach ($dir in Get-ChildItem -Path "_addonL10n/$addonId" -Directory) {
 } # End foreach
 
 # COMMIT CHANGES
-git config user.name "github-actions[bot]"
-git config user.email "github-actions[bot]@users.noreply.github.com"
-
 git add addon/locale addon/doc
-
 git diff --staged --quiet
 if ($LASTEXITCODE -ne 0) {
     git commit -m "Update translations for $addonId from Crowdin"
@@ -95,37 +139,4 @@ if ($LASTEXITCODE -ne 0) {
     git push -f --set-upstream origin $env:downloadTranslationsBranch
 } else {
     Write-Host "Nothing to commit."
-}
-
-# Update xliff
-$xlifFile = "$addonId.xliff"
-$mdFile = "./readme.md"
-if (Test-Path $mdFile) {
-    if (Test-Path $xliffFile) {
-        $tempXliff = [System.IO.Path]::GetTempFileName()
-        Copy-Item "$addonId.xliff" $tempXliff -Force
-        Write-Host "Copied $addonId.xliff to temporary file: $tempXliff"
-        uv run .github/scripts/markdownTranslate.py updateXliff -m $mdFile -x $tempXliff -o $xliffFile  
-        Write-Host "Updated $xlifFile based on $mdFile"
-    } else {
-        Write-Host "XLIFF file not found, but readme.md exists. Creating an XLIFF template for translations."
-        uv run .github/scripts/markdownTranslate.py generateXliff -m $mdFile -o $xliffFile
-    } else {
-        Write-Host "readme.md not found. Skipping XLIFF generation."
-}
-
-# Update pot file
-scons pot
-$potFile = "$addonId.pot"
-if (Test-Path $potFile) {
-    Write-Host "Uploading updated POT to Crowdin..."
-    ./l10nUtil.exe uploadSourceFile "$potFile" -c addon
-} else {
-    Write-Host "POT file not found, skipping POT update."
-}
-if (Test-Path $xliffFile) {
-    Write-Host "Uploading XLIFF to Crowdin..."
-    ./l10nUtil.exe uploadSourceFile "$xliffFile" -c addon
-} else {
-    Write-Host "XLIFF file not found, skipping XLIFF upload."
 }
