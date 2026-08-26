69126e4ed8824f880b8804bfe42e87823c6c8997
Noelia Ruiz Martínez <nrm1977@gmail.com>
2026-08-26 23:16:24 +0200

    Prepare for 3.0.0 version

--- buildVars.py
+++ buildVars.py
@@ -10,45 +10,43 @@ from site_scons.site_tools.NVDATool.typings import AddonInfo, BrailleTables, Sym
 # which returns whatever is given to it as an argument.
 from site_scons.site_tools.NVDATool.utils import _
 
-
 # Add-on information variables
 addon_info = AddonInfo(
-    # add-on Name/identifier, internal for NVDA
-    addon_name="readonlyProfiles",
-    # Add-on summary, usually the user visible name of the addon.
-    # Translators: Summary for this add-on
-    # to be shown on installation and add-on information found in Add-ons Manager.
-    addon_summary=_("Readonly Profiles"),
-    # Add-on description
-    # Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-    addon_description=_("""Don't save profiles automatically. Press NVDA+shift+p to save the most recent profile."""),
-    # version
-    addon_version="2.1.0",
-    # Brief changelog for this version
-    # Translators: what's new content for the add-on version to be shown in the add-on store
-    addon_changelog=_("""* Updates to translations."""),
-    # Author(s)
-    addon_author="Noelia Ruiz Martínez <nrm1977@gmail.com>",
-    # URL for the add-on documentation support
-    addon_url="https://github.com/nvdaes/readonlyProfiles",
-    # URL for the add-on repository where the source code can be found
-    addon_sourceURL="https://github.com/nvdaes/readonlyProfiles",
-    # Documentation file name
-    addon_docFileName="readme.html",
-    # Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-    addon_minimumNVDAVersion="2026.1",
-    # Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-    addon_lastTestedNVDAVersion="2026.2",
-    # Add-on update channel (default is None, denoting stable releases,
-    # and for development releases, use "dev".)
-    # Do not change unless you know what you are doing!
-    addon_updateChannel=None,
-    # Add-on license such as GPL 2
-    addon_license=None,
-    # URL for the license document the ad-on is licensed under
-    addon_licenseURL=None,
+	# add-on Name/identifier, internal for NVDA
+	addon_name='readonlyProfiles',
+	# Add-on summary/title, usually the user visible name of the add-on
+	# Translators: Summary/title for this add-on
+	# to be shown on installation and add-on information found in add-on store
+	addon_summary=_('Readonly Profiles'),
+	# Add-on description
+	# Translators: Long description to be shown for this add-on on add-on information from add-on store
+	addon_description=_("Don't save profiles automatically. Press NVDA+shift+p to save the most recent profile."),
+	# version
+	addon_version="2.1.0",
+	# Brief changelog for this version
+	# Translators: what's new content for the add-on version to be shown in the add-on store
+	addon_changelog=_('* Updates to translations.'),
+	# Author(s)
+	addon_author='Noelia Ruiz Martínez <nrm1977@gmail.com>',
+	# URL for the add-on documentation support
+	addon_url='https://github.com/nvdaes/readonlyProfiles',
+	# URL for the add-on repository where the source code can be found
+	addon_sourceURL='https://github.com/nvdaes/readonlyProfiles',
+	# Documentation file name
+	addon_docFileName='readme.html',
+	# Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
+	addon_minimumNVDAVersion='2026.1',
+	# Last NVDA version supported/tested (e.g. "2024.4.0", ideally more recent than minimum version)
+	addon_lastTestedNVDAVersion='2026.2',
+	# Add-on update channel (default is None, denoting stable releases,
+	# and for development releases, use "dev".)
+	# Do not change unless you know what you are doing!
+	addon_updateChannel=None,
+	# Add-on license such as GPL 2
+	addon_license=None,
+	# URL for the license document the ad-on is licensed under
+	addon_licenseURL=None,
 )
-
 
 # Define the python files that are the sources of your add-on.
 # You can either list every file (using ""/") as a path separator,
@@ -58,10 +56,7 @@ addon_info = AddonInfo(
 # pythonSources = ["addon/globalPlugins/*.py"]
 # For more information on SCons Glob expressions please take a look at:
 # https://scons.org/doc/production/HTML/scons-user/apd.html
-pythonSources: list[str] = [
-    "addon/*.py",
-    "addon/globalPlugins/*.py",
-]
+pythonSources: list[str] = ['addon/*.py', 'addon/globalPlugins/*.py']
 
 # Files that contain strings for translation. Usually your python sources
 i18nSources: list[str] = pythonSources + ["buildVars.py"]
@@ -76,7 +71,7 @@ excludedFiles: list[str] = []
 # If your add-on is written in a language other than english, modify this variable.
 # For example, set baseLanguage to "es" if your add-on is primarily written in spanish.
 # You must also edit .gitignore file to specify base language files to be ignored.
-baseLanguage: str = "en"
+baseLanguage: str = 'en'
 
 # Markdown extensions for add-on documentation
 # Most add-ons do not require additional Markdown extensions.
@@ -86,7 +81,6 @@ baseLanguage: str = "en"
 markdownExtensions: list[str] = []
 
 # Custom braille translation tables
-# Table files reside in the brailleTables folder and are named `tableName.ctb` (or .utb, .dis).
 # If your add-on includes custom braille tables (most will not), fill out this dictionary.
 # Each key is a dictionary named according to braille table file name,
 # with keys inside recording the following attributes:
